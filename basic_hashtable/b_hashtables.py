fe35c2a8e70032def067d01297e230a313824e55
Erin Koen <erin.koen@gmail.com>
2019-07-08 17:32:49 -0400

    commented out polya-wise on hashtables.py

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -1,4 +1,4 @@
-
+# takes a value, runs a hashing operation on it, 
 
 # '''
 # Basic hash table key/value pair
@@ -15,7 +15,9 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.count = 0 # not sure we'll need this
+        self.elements = [None] * capacity 
 
 
 # '''
@@ -23,7 +25,13 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    # take a string, run it through hash function, return an integer between 0 (inclusive) and max (exclusive). Max will be passed as BasicHashTable.capacity
+    djb2 = 5381
+    for c in string:
+        djb2 = (hash*33)+ ord(c)
+
+    return djb2 % max # will provide the index in which to place a new value, delete or edit an existing one
+
 
 
 # '''
@@ -32,6 +40,10 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
+    # hash the key
+    # check capacity vs count and resize array if needed
+    # insert the value at hash_table[hashed_key], handle errors
+    # increment count
     pass
 
 
@@ -41,6 +53,8 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
+    # hash the key
+    # remove the value at hash_table[hashed_key], handle errors
     pass
 
 
@@ -50,6 +64,7 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
+    # hash key - if hash_table[hashed_key], return value stored there. Else return None.
     pass
 
 
