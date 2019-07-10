e7be95c5901c4dc0cdc814c5f5306ab615e2db1c
Erin Koen <erin.koen@gmail.com>
2019-07-10 10:26:56 -0400

    everything up to resizing done

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -18,7 +18,8 @@ class LinkedPair:
 class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = [] * capacity
+        self.storage = [None] * capacity
+        self.count = 0
 
 
 # '''
@@ -28,9 +29,10 @@ def hash(string, max_index):
     # take a string, run it through hash function, return an integer between 0 (inclusive) and max (exclusive). Max will be passed as BasicHashTable.capacity
     djb2 = 5381
     for char in string:
-        djb2 = (djb2*33)+ ord(char)
+        djb2 = (djb2*33) + ord(char)
 
-    return djb2 % max_index # will provide the index in which to place a new value, delete or edit an existing one
+    # will provide the index in which to place a new value, delete or edit an existing one
+    return djb2 % max_index
 
 
 # '''
@@ -40,40 +42,69 @@ def hash(string, max_index):
 # '''
 def hash_table_insert(hash_table, key, value):
     # create a linkedPair
-    element = LinkedPair(key,value)
+    element = LinkedPair(key, value)
 
     # hash the key to find the index
     idx = hash(key, hash_table.capacity)
 
-    # check storage at index to see if there's a collision
+
+    # check storage at index to see if there's a collision, and just insert the elemnt if the index is empty
     if hash_table.storage[idx] is None:
-        hash_table.storage[idx] =  element
+        hash_table.storage[idx] = element
         return
     
-    else:
-        for e in hash_table.storage:
-            if e.next is None:
-                e.next = element
-                return
+    # otherwise traverse till you find an empty spot
+    curr_node = hash_table.storage[idx]
+    while curr_node.next is not None:
+        curr_node = curr_node.next
+    
+    # and stick in the element when you get there
+    curr_node.next = element
 
 
-
-# '''
-# Fill this in.
-
-# If you try to remove a value that isn't there, print a warning.
-# '''
 def hash_table_remove(hash_table, key):
-    pass
+    # hash the key to find the index
+    idx = hash(key, hash_table.capacity)
+    # if there's nothing there, print it out and let 'em know
+    if hash_table.storage[idx] is None:
+        print(f'There is no key: value pair at that index.')
+        return None
+    # if there's only one ListNode, and its key is equal to that which was passed, set it to None
+    if hash_table.storage[idx].next is None and hash_table.storage[idx].key == key:
+        hash_table.storage[idx] = None
+        print(f'The item was found and deleted.')
+        return None
 
+    # set a variable to track the node you're traversing TO, start with the second node, since we've satisfied the conditionals above
+    curr_node = hash_table.storage[idx].next
 
-# '''
-# Fill this in.
+    # figure out how to traverse through the LL till you find the key
+    while curr_node.next.key is not key:
+
+        # set curr node to the next node
+        curr_node = curr_node.next
+
+        # if you get to the end of the list, print an error and return
+        if curr_node.next is None:
+            print(f'No pair with that key was found.')
+            return
+
+    # if you get this far it's because the next node's key is what you're looking for. Change the reference in curr_node.next to point to the pair two spots ahead of it
+    curr_node.next = curr_node.next.next
+
 
 # Should return None if the key is not found.
-# '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    # hash the key to find the index
+    idx = hash(key, hash_table.capacity)
+    curr_node = hash_table.storage[idx]
+    while curr_node.key is not key:
+        if curr_node.next is None:
+            print(f'That key does not exist.')
+            return None
+        curr_node = curr_node.next
+    # if you've gotten here it's because the current node is the one you're looking for. Return it.
+    return curr_node
 
 
 # '''
@@ -94,12 +125,12 @@ def Testing():
     print(hash_table_retrieve(ht, "line_2"))
     print(hash_table_retrieve(ht, "line_3"))
 
-    old_capacity = len(ht.storage)
+    # old_capacity = len(ht.storage)
     # ht = hash_table_resize(ht)
-    new_capacity = len(ht.storage)
+    # new_capacity = len(ht.storage)
 
-    print("Resized hash table from " + str(old_capacity)
-          + " to " + str(new_capacity) + ".")
+    # print("Resized hash table from " + str(old_capacity)
+    #       + " to " + str(new_capacity) + ".")
 
 
 Testing()
